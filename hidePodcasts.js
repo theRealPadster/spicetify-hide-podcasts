65befd59dd7329523ce3b5c5bfce4ef9ecb0e79c
Isaac Maier <padspambox@gmail.com>
2021-01-20 22:13:40 -0500

    ✨♻️ Remove Podcasts tab from Browse screen... (Refactor extension to keep track of documents and apply changes to all documents)

--- hidePodcasts.js
+++ hidePodcasts.js
@@ -13,73 +13,101 @@
         return;
     }
 
+    // Store the app's documents (main document and grab Browse iframe if possible)
+    let documents = getDocuments();
     const SETTINGS_KEY = 'HidePodcastsMode';
-
-    injectCSS();
-    tagItems();
-
     let isEnabled = LocalStorage.get(SETTINGS_KEY) === '1';
-    setState(isEnabled);
+
+    setState(documents, isEnabled);
+    injectCSS(documents);
+    tagItems(documents);
 
     // Add menu item and menu click handler
     new Menu.Item('Hide podcasts', isEnabled, (self) => {
         isEnabled = !isEnabled;
         LocalStorage.set(SETTINGS_KEY, isEnabled ? '1' : '0');
         self.setState(isEnabled);
-        setState(isEnabled);
+        setState(documents, isEnabled);
     }).register();
 
     Player.addEventListener('appchange', () => {
         if (!isEnabled) return;
-        tagItems();
+        // Refresh documents (to try and grab Browse frame)
+        documents = getDocuments();
+
+        setState(documents, isEnabled);
+        injectCSS(documents);
+        tagItems(documents);
     });
 })();
 
-// Inject the css that allows us to toggle podcasts
-function injectCSS() {
-    let style = document.createElement('style');
-    style.innerHTML =
-    `
-    .hide-podcasts-enabled .podcast-item {
-        display: none;
-    }
-    `;
-    document.querySelector('body').appendChild(style);
-}
+// Helpers
 
-// Add our class to any podcast elements
-function tagItems() {
-    // Remove podcast sidebar link (only needs to run once)
-    let podcastSidebarItem = document.querySelector('.SidebarListItemLink[href="spotify:app:collection:podcasts"]');
-    if (podcastSidebarItem) podcastSidebarItem.closest('.SidebarListItem').classList.add('podcast-item');
-
-    // Run on app change
-    // Remove podcast carousels (e.g. 'Home' and 'Made For You')
-    let carousels = document.querySelectorAll('.Carousel');
-    carousels.forEach(carousel => {
-        let title = carousel.querySelector('.GlueSectionDivider__title');
-        title = title ? title.innerText : '';
-
-        // It seems to tag podcast items with the Card--show class
-        let podcastCards = carousel.querySelectorAll('.Card.Card--show');
-        if (podcastCards.length > 0) {
-            console.log(`Tagging carousel: ${title}`);
-            carousel.classList.add('podcast-item');
-        }
-    });
-
-    // Remove podcast tab from Browse
+function getDocuments() {
+    let documents = [document];
     let browseFrame = document.getElementById('app-browse');
     if (browseFrame) {
-        let doc = browseFrame.contentDocument;
+        let browseDocument = browseFrame.contentDocument;
+        documents.push(browseDocument);
+    }
+    return documents;
+}
+
+// Inject the css that allows us to toggle podcasts
+function injectCSS(documents) {
+    documents.forEach(doc => {
+        let body = doc.querySelector('body');
+
+        // Inject style if it doesnt have it already
+        if (!body.classList.contains('hide-podcasts--style-injected')) {
+            let style = doc.createElement('style');
+            style.innerHTML =
+            `
+            .hide-podcasts-enabled .podcast-item {
+                display: none !important;
+            }
+            `;
+            body.appendChild(style);
+            body.classList.add('hide-podcasts--style-injected');
+        }
+    });
+}
+
+
+// Add our class to any podcast elements
+function tagItems(documents) {
+    documents.forEach(doc => {
+
+        // Remove podcast sidebar link (only needs to run once)
+        let podcastSidebarItem = doc.querySelector('.SidebarListItemLink[href="spotify:app:collection:podcasts"]');
+        if (podcastSidebarItem) podcastSidebarItem.closest('.SidebarListItem').classList.add('podcast-item');
+
+        // Run on app change
+        // Remove podcast carousels (e.g. 'Home' and 'Made For You')
+        let carousels = doc.querySelectorAll('.Carousel');
+        carousels.forEach(carousel => {
+            let title = carousel.querySelector('.GlueSectionDivider__title');
+            title = title ? title.innerText : '';
+
+            // It seems to tag podcast items with the Card--show class
+            let podcastCards = carousel.querySelectorAll('.Card.Card--show');
+            if (podcastCards.length > 0) {
+                console.log(`Tagging carousel: ${title}`);
+                carousel.classList.add('podcast-item');
+            }
+        });
+
+        // Remove podcast tab from Browse (only in browse frame document)
         let tab = doc.body.querySelector('[data-navbar-item-id="spotify:app:browse:podcasts"]');
         if (tab) tab.classList.add('podcast-item');
-    }
+    });
 }
 
 // Add/remove the body class that hides podcasts
-function setState(isEnabled) {
-    let body = document.querySelector('body');
-    if (isEnabled) body.classList.add('hide-podcasts-enabled');
-    else body.classList.remove('hide-podcasts-enabled');
+function setState(documents, isEnabled) {
+    documents.forEach(doc => {
+        let body = doc.querySelector('body');
+        if (isEnabled) body.classList.add('hide-podcasts-enabled');
+        else body.classList.remove('hide-podcasts-enabled');
+    });
 }
