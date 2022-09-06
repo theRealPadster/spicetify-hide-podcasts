bb4cbb8401a5ecccf2af6efae9b04fae55d0d8a4
Isaac Maier <padspambox@gmail.com>
2022-09-05 21:48:30 -0400

    🇩🇪 Enable German locale

--- src/app.tsx
+++ src/app.tsx
@@ -8,6 +8,7 @@
 import i18n from 'i18next';
 import en from './locales/en.json';
 import fr from './locales/fr.json';
+import de from './locales/de.json';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
@@ -21,6 +22,7 @@ i18n
     resources: {
       en,
       fr,
+      de,
     },
     detection: {
       order: [ 'navigator', 'htmlTag' ],
