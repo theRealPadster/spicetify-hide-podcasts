22187e069401e71a7e8e6d783fbcedf4eb14d4da
Isaac Maier <padspambox@gmail.com>
2023-12-09 18:54:51 -0500

    Don't manually inject css

--- src/app.tsx
+++ src/app.tsx
@@ -22,6 +22,8 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import { getLocalStorageDataFromKey, getPageLoadedSelector } from './util';
 
+import './css/app.scss';
+
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector)
@@ -52,35 +54,6 @@ i18n
 const SETTINGS_KEY = 'HidePodcastsEnabled';
 const AGGRESSIVE_MODE_KEY = 'HidePodcastsAggressiveMode';
 const AUDIOBOOKS_KEY = 'HidePodcastsHideAudioBooks';
-
-/** Inject the css that allows us to toggle podcasts and audiobooks */
-const injectCSS = () => {
-  const body = document.body;
-
-  // Inject style if it doesnt have it already
-  if (!body.classList.contains('hide-podcasts--style-injected')) {
-    const style = document.createElement('style');
-    // TODO: Need to add the queue-tabBar bit to block the Podcasts tab on the Your Library page
-    // Because it resets itself when the window resizes and it go in/out of the overflow menu
-    // Technically I should block the li.queue-tabBar-headerItem above it, but can't do that with just CSS
-
-    style.innerHTML =
-      // General rules
-      `.hide-podcasts-enabled .podcast-item {
-        display: none !important;
-      }
-      .hide-audiobooks-enabled .audiobook-item {
-        display: none !important;
-      }`
-      + // Podcasts tab in Your Library page
-      `.hide-podcasts-enabled .queue-tabBar-header a[href="/collection/podcasts"] {
-        display: none !important;
-      }`;
-    style.className = 'hide-podcasts--style';
-    document.head.appendChild(style);
-    body.classList.add('hide-podcasts--style-injected');
-  }
-};
 
 /** Add our class to any podcast elements */
 const tagPodcasts = () => {
@@ -196,7 +169,6 @@ async function main() {
   // Run the app logic
   function apply() {
     setState({ podcasts: isEnabled, audiobooks: hideAudioBooks });
-    injectCSS();
     tagPodcasts();
     tagAudioBooks();
   }
